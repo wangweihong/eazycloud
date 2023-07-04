911c886db16cd3344eaf3035bc5b9a2c252e1fc2
wangweihong <wangweihong2018@qq.com>
2023-07-04 11:48:49 +0000

    feat(tracectx): support traced id pkg

---
+++ pkg/tracectx/trace.go
@@ -0,0 +1,21 @@
+package tracectx
+
+import (
+	"fmt"
+	"os"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	incrNum uint64
+	pid     = os.Getpid()
+)
+
+// NewTraceID generate trace id.
+func NewTraceID() string {
+	return fmt.Sprintf("trace-id-%d-%s-%d",
+		pid,
+		time.Now().Format("2006.01.02.15.04.05.999"),
+		atomic.AddUint64(&incrNum, 1))
+}
