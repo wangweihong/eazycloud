a7e1ee8f536688b5a8023007c518f7e5909e98db
wangweihong <wangweihong2018@qq.com>
2023-08-09 07:31:33 +0000

    test(grpc): add test command for different tls env

--- test/grpc/tcp/test_panic.sh
+++ test/grpc/tcp/test_panic.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # gRPC  server must enable reflect and debug service
-# 可使用-plaintext 忽略证书检测
-grpcurl --cacert=./_output/cert/ca.crt debug.DebugService/Panic
+grpcurl -plaintext debug.DebugService/Panic
+
--- test/grpc/tcp/test_version.sh
+++ test/grpc/tcp/test_version.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+grpcurl --cacert=./_output/cert/ca.crt debug.DebugService/Panic
+# 服务未开启TLS时
 grpcurl -plaintext localhost:8081  version.VersionService/Version
