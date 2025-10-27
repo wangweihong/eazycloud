8f64740f85e316828618645383d455d9a32cbd88
wangweihong <wangweihong2018@qq.com>
2025-10-27 20:07:10 +0800

    remove example

--- examples/httpcli/example/options/example.go
+++
@@ -1,6 +0,0 @@
-package options
-
-// BackendOptions contains configuration items related to server features.
-type BackendOptions struct {
-	Address string `json:"address" mapstructure:"address"`
-}
