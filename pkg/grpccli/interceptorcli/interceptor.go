559d8cfe9cfbf8847b5e46c119427849893dbbb1
wangweihong <wangweihong2018@qq.com>
2025-10-27 20:37:59 +0800

    fix grpccli

--- pkg/grpccli/interceptorcli/interceptor.go
+++ pkg/grpccli/interceptorcli/interceptor.go
@@ -1,20 +1,40 @@
-package tracing
+package interceptorcli
 
-//
-//// UnaryClientInterceptor returns a new unary client interceptor for logging.
-//func UnaryClientInterceptor(skipperFunc ...skipper.SkipperFunc) grpc.UnaryClientInterceptor {
-//	tracer := otel.GetTracerProvider().Tracer("tracer")
-//
-// 	return func(ctx context.Context, method string, req, reply any, cc *grpc.ClientConn, invoker
-// grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-//		name,attr,_:= mtrace.
-//		return nil
-//	}
-//}
-//
-//func StreamClientInterceptor() grpc.StreamClientInterceptor {
-// 	return func(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer,
-// opts ...grpc.CallOption) (grpc.ClientStream, error) {
-//		return nil, nil
-//	}
-//}
+import (
+	"google.golang.org/grpc"
+
+	"github.com/wangweihong/eazycloud/pkg/grpccli/interceptorcli/callstatus"
+	"github.com/wangweihong/eazycloud/pkg/grpccli/interceptorcli/logging"
+
+	"github.com/wangweihong/gotoolbox/pkg/skipper"
+)
+
+const (
+	InterceptorNameLogger     = "logger"
+	InterceptorNameCallStatus = "callstatus"
+)
+
+var (
+	UnaryClientInterceptorList  = defaultUnaryClientInterceptorList()
+	UnaryClientInterceptorNames = defaultInterceptorListNames()
+)
+
+func defaultUnaryClientInterceptorList(skipperFunc ...skipper.SkipperFunc) map[string]grpc.UnaryClientInterceptor {
+	return map[string]grpc.UnaryClientInterceptor{
+		InterceptorNameLogger:     logging.UnaryClientInterceptor(skipperFunc...),
+		InterceptorNameCallStatus: callstatus.UnaryClientInterceptor(skipperFunc...),
+	}
+}
+
+func defaultInterceptorListNames() []string {
+	names := make([]string, 0, len(defaultUnaryClientInterceptorList()))
+	for name := range defaultUnaryClientInterceptorList() {
+		names = append(names, name)
+	}
+	return names
+}
+
+// 生成带有跳过条件的拦截器列表.
+func GetUnaryClientInterceptorWithSkippers(skipperFunc ...skipper.SkipperFunc) map[string]grpc.UnaryClientInterceptor {
+	return defaultUnaryClientInterceptorList(skipperFunc...)
+}
